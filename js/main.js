a488f06cb369e742aab8d43804db0fc3961835ab
amw2026 <amw.conference@gmail.com>
2026-08-26 05:45:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -103,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function() {
             e.preventDefault(); // Bloquea redirecciones
             e.stopPropagation(); // Evita clics accidentales debajo del menú
 
-            if (window.innerWidth <= 1054) {
+            if (window.innerWidth <= 1055) {
                 // Selecciona el submenú específico que está justo debajo de ESTE botón
                 const dropdownMenu = this.nextElementSibling;
                 
